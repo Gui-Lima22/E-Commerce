8400f115be2dfa6e9cccfce8e9154f5aa2f6645a
BotGui <botgui2208@gmail.com>
2023-09-06 22:05:20 -0300

    first commit

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,33 @@
+<!
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/mdb/css/mdb.min.css') }}">
+    <title>PhoenixShop</title>
+</head>
+<body>
+<div class="title mt-2">
+    <div class="col-3 text-center">
+        <h3 class="actual-title title">PhoenixShop</h3>
+    </div>
+    <div class="col-9">
+
+    </div>
+</div>
+<hr/>
+<div class="root mt-3" id="root">
+    <div class="row col-12">
+
+    </div>
+</div>
+
+</body>
+<script src="{{ asset("plugins/mdb/js/mdb.min.js") }}"></script>
+<script src="{{ asset("js/individual/index.js") }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+</html>
